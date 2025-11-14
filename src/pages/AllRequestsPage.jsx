6cb16adf84bb0eefdde2a1ab6bb0f8344f124260
mhaider1129 <m.haider1129@gmail.com>
2025-11-15 00:00:30 +0300

    Describe what you changed here

--- src/pages/AllRequestsPage.jsx
+++ src/pages/AllRequestsPage.jsx
@@ -677,12 +677,13 @@ const AllRequestsPage = () => {
           onChange={(e) => setToDate(e.target.value)}
         />
 
-                <select className="border p-2 rounded" value={status} onChange={(e) => setStatus(e.target.value)}>
+        <select className="border p-2 rounded" value={status} onChange={(e) => setStatus(e.target.value)}>
           <option value="">All Statuses</option>
           <option value="Pending">Pending</option>
           <option value="Approved">Approved</option>
+          <option value="Received">Received</option>
           <option value="Rejected">Rejected</option>
-          <option value="completed">Completed</option>
+          <option value="Completed">Completed</option>
         </select>
 
         <select className="border p-2 rounded" value={department} onChange={(e) => setDepartment(e.target.value)}>
